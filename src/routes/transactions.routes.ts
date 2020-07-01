7d8bcfe165e6fe048969065f0d8ed07da1a884db
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2020-07-01 08:02:11 -0300

    Updating GET route and CreateTransactionService

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-
+import { getCustomRepository } from 'typeorm';
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
@@ -7,14 +7,21 @@ import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionsRouter = Router();
 
-transactionsRouter.get('/', async (request, response) => {});
+transactionsRouter.get('/', async (request, response) => {
+  const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+  const transactions = await transactionsRepository.find();
+  const balance = await transactionsRepository.getBalance();
+
+  return response.json({ transactions, balance });
+});
 
 transactionsRouter.post('/', async (request, response) => {
   const { title, value, type, category } = request.body;
 
   const createTransaction = new CreateTransactionService();
 
-  const transaction = await createTransaction({
+  const transaction = await createTransaction.execute({
     title,
     value,
     type,
