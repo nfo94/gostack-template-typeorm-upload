ca9c404a8b15c3d657bdc05bbd499f6da451097c
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2020-07-01 22:03:26 -0300

    Setting multer and upload configuration in transactions.router

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import { getCustomRepository } from 'typeorm';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
 import DeleteTransactionService from '../services/DeleteTransactionService';
 import ImportTransactionsService from '../services/ImportTransactionsService';
+
+const upload = multer(uploadConfig);
 
 const transactionsRouter = Router();
 
@@ -37,11 +41,9 @@ transactionsRouter.delete('/:id', async (request, response) => {
   const deleteTransaction = new DeleteTransactionService();
   await deleteTransaction.execute(id);
 
-  return response.status(204).send();
+  return response.status(204).send;
 });
 
-transactionsRouter.post('/import', async (request, response) => {
-  // TODO
-});
+transactionsRouter.post('/import', async (request, response) => {});
 
 export default transactionsRouter;
