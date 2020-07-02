6e76b61e14230a368160a03de7d87a4e3d35a5e3
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2020-07-01 22:06:52 -0300

    Updating POST method for upload

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -44,6 +44,16 @@ transactionsRouter.delete('/:id', async (request, response) => {
   return response.status(204).send;
 });
 
-transactionsRouter.post('/import', async (request, response) => {});
+transactionsRouter.post(
+  '/import',
+  upload.single('file'),
+  async (request, response) => {
+    const importTransactions = new ImportTransactionsService();
+
+    const transactions = await importTransactions.execute(request.file.path);
+
+    return response.json(transactions);
+  },
+);
 
 export default transactionsRouter;
